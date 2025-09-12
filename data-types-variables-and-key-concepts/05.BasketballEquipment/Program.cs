5c4c800109f04d256f73a5f4081f144315e931ad
MariyaSpasova-Git <mariyaspasova3@gmail.com>
2025-09-12 21:56:12 +0300

    Add homework of 6 problems for data types, variables, and key concepts

---
+++ data-types-variables-and-key-concepts/05.BasketballEquipment/Program.cs
@@ -0,0 +1,10 @@
+﻿int annualFee = int.Parse(Console.ReadLine());
+
+double sneakerCost = annualFee * 0.60;
+double uniformCost = sneakerCost * 0.80;
+double ballCost = uniformCost / 4;
+double accessories = ballCost / 5;
+
+double totalCost = annualFee + sneakerCost + uniformCost + ballCost + accessories;
+
+Console.WriteLine(totalCost);
